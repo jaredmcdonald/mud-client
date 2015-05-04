6ae407d34c1c18e1730bd2dde8d56f37c00613c9
Jared McDonald <jared.mcdonald@jcrew.com>
2015-05-04 16:25:12 -0400

    Revert "use telnet library"
    This reverts commit 522a46cedb91bf0421f21ae3e9afab0f0fc5dfd1.

--- socket/socket.js
+++ socket/socket.js
@@ -1,54 +1,25 @@
-// var net = require('net');
-var telnet = require('telnet-client');
+var net = require('net');
 var socketio = require('socket.io');
-var params = {
-  host: 'avatar.outland.org',
-  port: 3000
-}
-
 
 function onConnect (websocket) {
-  var connection = new telnet();
-
-  connection.on('ready', function (prompt) {
-    websocket.on('data', function (data) {
-      connection.exec(data, function (response) {
-        websocket.emit('data', response);
-      });
-    });
+  var client = new net.Socket({
+    writeable: true
   });
-
-  connection.on('timeout', function() {
-    console.log('socket timeout!')
-    connection.end();
+  client.on('data', function (data) {
+    websocket.emit('data', data.toString());
   });
-
-  connection.on('close', function() {
-    console.log('connection closed');
+  websocket.on('data', function (data) {
+    client.write(data);
   });
-
-  connection.connect(params);
-
-
-  // var client = new net.Socket({
-  //   writeable: true,
-  //   readable : true
-  // });
-  // client.on('data', function (data) {
-  //   websocket.emit('data', data.toString());
-  // });
-  // websocket.on('data', function (data) {
-    // client.write(data);
-  // });
-  // client.on('close', function () {
-  //   websocket.emit('Connection closed by foreign host.');
-  // });
-  // websocket.on('disconnect', function () {
-    // client.destroy();
-  // });
-  // client.connect(3000, 'avatar.outland.org', function () {
-  //   console.log('connected');
-  // });
+  client.on('close', function () {
+    websocket.emit('Connection closed by foreign host.');
+  });
+  websocket.on('disconnect', function () {
+    client.destroy();
+  });
+  client.connect(3000, 'avatar.outland.org', function () {
+    console.log('connected');
+  });
 }
 
 module.exports = function (server) {
