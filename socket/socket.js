522a46cedb91bf0421f21ae3e9afab0f0fc5dfd1
Jared McDonald <jared.mcdonald@jcrew.com>
2015-05-04 16:14:20 -0400

    use telnet library

--- socket/socket.js
+++ socket/socket.js
@@ -1,25 +1,54 @@
-var net = require('net');
+// var net = require('net');
+var telnet = require('telnet-client');
 var socketio = require('socket.io');
+var params = {
+  host: 'avatar.outland.org',
+  port: 3000
+}
+
 
 function onConnect (websocket) {
-  var client = new net.Socket({
-    writeable: true
+  var connection = new telnet();
+
+  connection.on('ready', function (prompt) {
+    websocket.on('data', function (data) {
+      connection.exec(data, function (response) {
+        websocket.emit('data', response);
+      });
+    });
   });
-  client.on('data', function (data) {
-    websocket.emit('data', data.toString());
+
+  connection.on('timeout', function() {
+    console.log('socket timeout!')
+    connection.end();
   });
-  websocket.on('data', function (data) {
-    client.write(data);
+
+  connection.on('close', function() {
+    console.log('connection closed');
   });
-  client.on('close', function () {
-    websocket.emit('Connection closed by foreign host.');
-  });
-  websocket.on('disconnect', function () {
-    client.destroy();
-  });
-  client.connect(3000, 'avatar.outland.org', function () {
-    console.log('connected');
-  });
+
+  connection.connect(params);
+
+
+  // var client = new net.Socket({
+  //   writeable: true,
+  //   readable : true
+  // });
+  // client.on('data', function (data) {
+  //   websocket.emit('data', data.toString());
+  // });
+  // websocket.on('data', function (data) {
+    // client.write(data);
+  // });
+  // client.on('close', function () {
+  //   websocket.emit('Connection closed by foreign host.');
+  // });
+  // websocket.on('disconnect', function () {
+    // client.destroy();
+  // });
+  // client.connect(3000, 'avatar.outland.org', function () {
+  //   console.log('connected');
+  // });
 }
 
 module.exports = function (server) {
