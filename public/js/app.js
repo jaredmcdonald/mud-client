ddbffd58e3267c9fc67f5f162d33815adb7ff529
Jared McDonald <jared.r.mcdonald@gmail.com>
2015-05-04 21:03:43 -0400

    styling, scrollback, HTML escaping

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,53 @@
 document.addEventListener('DOMContentLoaded', function () {
+  'use strict';
+
+  var SCROLLBACK_LINES = 4000;
+  var CLEAN_INTERVAL = 20000;
 
   var socket = io.connect();
   var el = document.getElementById('content');
   var userInput = document.getElementById('userInput');
 
+  // Render something and update scroll position
   function renderText (text) {
     el.innerHTML += text;
+    updateScroll();
+  }
+
+  // Keep scroll up to date with input
+  function updateScroll () {
+    el.scrollTop = el.scrollHeight;
+  }
+
+  // Clean up scrollback
+  function cleanScrollback () {
+    var arr = el.innerHTML.split('\n');
+    if (arr.length <= SCROLLBACK_LINES) {
+      return false;
+    };
+    el.innerHTML = arr.slice(arr.length - SCROLLBACK_LINES).join('\n');
+  }
+
+  // handle user input
+  function handleInput (event) {
+    event.preventDefault();
+
+    var input = event.target[0];
+    var val = input.value;
+
+    socket.emit('data', val);
+
+    input.value = '';
+    renderText('\n&gt; ' + val + '\n\n');
+    updateScroll();
   }
 
   socket.on('data', renderText);
 
-  userInput.addEventListener('submit', function (event) {
-    event.preventDefault();
-    var val = event.target[0].value;
-    if (!val) return false;
+  window.setInterval(cleanScrollback, CLEAN_INTERVAL);
 
-    renderText('\n> ' + val);
-    socket.emit('data', val);
-    event.target[0].value = '';
-  });
+  userInput.addEventListener('submit', handleInput);
+
+  userInput[0].focus();
 
 });
--- socket/socket.js
+++ socket/socket.js
@@ -5,30 +5,38 @@ var socketio = require('socket.io');
 var ansi_up = require('ansi_up');
 var hostname = process.argv[2] || process.env.REMOTE_HOSTNAME;
 var port = process.argv[3] || process.env.REMOTE_PORT;
+var ansiConfig = {
+  use_classes: true
+};
+
+function processText (data) {
+  return ansi_up.ansi_to_html(ansi_up.escape_for_html(data.toString()),
+    ansiConfig);
+}
 
 function onConnect (websocket) {
   var client = new net.Socket({
     readable: true,
     writeable: true
   });
+
   client.on('data', function (data) {
-    websocket.emit('data', ansi_up.ansi_to_html(data.toString(), {
-      use_classes: true
-    }));
+    websocket.emit('data', processText(data))
   });
+
   websocket.on('data', function (data) {
     client.write(data + '\n');
   });
+
   client.on('close', function () {
-    websocket.emit('Connection closed by foreign host.');
+    websocket.emit('data', 'Connection closed by foreign host.');
   });
+
   websocket.on('disconnect', function () {
     client.destroy();
   });
 
-  client.connect(port, hostname, function () {
-    console.log('connected');
-  });
+  client.connect(port, hostname);
 }
 
 module.exports = function (server) {
